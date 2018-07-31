8893f981a7db61de93025206dbd23d6085aa7e59
steveklabnik <steve@steveklabnik.com>
2018-07-31 12:10:52 -0400

    initial commit

---
+++ bar/src/main.rs
@@ -0,0 +1,13 @@
+#![feature(
+    async_await,
+    await_macro,
+    futures_api,
+    pin,
+    use_extern_macros
+)]
+#![feature(rust_2018_preview)]
+#![warn(rust_2018_idioms)]
+
+fn main() {
+    use foo::ref_slice;
+}
---
+++ foo/src/lib.rs
@@ -0,0 +1,11 @@
+#![feature(
+    async_await,
+    await_macro,
+    futures_api,
+    pin,
+    use_extern_macros
+)]
+#![feature(rust_2018_preview)]
+#![warn(rust_2018_idioms)]
+
+pub use ref_slice;
